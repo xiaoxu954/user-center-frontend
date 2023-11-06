c8a5cededbfa7e8dabe9f9a1e4e14ce8d8f0631e
xiaoxu954 <x17605049954@163.com>
2023-11-07 02:10:04 +0800

    优化路由，登陆界面默认为欢迎

--- config/routes.ts
+++ config/routes.ts
@@ -3,17 +3,19 @@ export default [
     path: '/user',
     layout: false,
     routes: [
-      { path: '/user/login', component: './user/Login' },
-      { path: '/user/register', component: './user/Register' },
+      { name: '登录', path: '/user/login', component: './user/Login' },
+      { name: '注册', path: '/user/register', component: './user/Register' },
       { component: './404' },
     ],
   },
+
   { name: '欢迎', path: '/welcome', icon: 'smile', component: './Welcome' },
   {
-    name: '用户管理',
+    name: '用户管理页面',
     path: '/admin',
     icon: 'crown',
     access: 'canAdmin',
+    //todo 管理员权限校验
     component: './Admin',
     routes: [
       {
@@ -35,5 +37,7 @@ export default [
   // {name: '查询表格', icon: 'table', path: '/list', component: './TableList'},
   // {name: '用户管理', path: '/admin/user-manage', icon: 'smile', component: './Admin/UserManage'},
 
+  //默认页面
+  { path: '/', redirect: '/welcome' },
   { component: './404' },
 ];
